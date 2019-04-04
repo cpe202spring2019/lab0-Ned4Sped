33a132566f15b2ee408a56eabf6daa3c0cd677b2
Ned4Sped <44121859+Ned4Sped@users.noreply.github.com>
2019-04-03 21:18:38 -0700

    Updated with proper functions

--- planets.py
+++ planets.py
@@ -1,7 +1,18 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+      # write your code here
+      #get weight on earth
+      earth_weight_string = input('What do you weigh on earth? ')
+      print('')
+
+      #change weight to an int
+      earth_weight = int(earth_weight_string)
+
+      #calc weight on other planets
+      mars_weight = 0.38 * earth_weight
+      jupiter_weight = 2.34 * earth_weight
+
+      #print output
+      print("On Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds." .format(mars_weight, jupiter_weight))
+
 if __name__ == '__main__':
-   weight_on_planets()
+      weight_on_planets()
